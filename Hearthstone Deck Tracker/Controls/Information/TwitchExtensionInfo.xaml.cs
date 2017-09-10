a146f94077f3a05ab2a567e46ce14359696130c5
Alexander Zeier <alex@hearthsim.net>
2017-09-10 13:02:49 +0200

    WIP twitch extension

---
+++ Hearthstone Deck Tracker/Controls/Information/TwitchExtensionInfo.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows.Input;
+using Hearthstone_Deck_Tracker.FlyoutControls.Options;
+using Hearthstone_Deck_Tracker.Utility;
+
+namespace Hearthstone_Deck_Tracker.Controls.Information
+{
+	public partial class TwitchExtensionInfo
+	{
+		public TwitchExtensionInfo()
+		{
+			InitializeComponent();
+		}
+
+		public ICommand OptionsCommand => new Command(() =>
+		{
+			AdvancedOptions.Instance.Show = true;
+			Core.MainWindow.Options.TreeViewItemStreamingTwitchExtension.IsSelected = true;
+			Core.MainWindow.FlyoutOptions.IsOpen = true;
+		});
+	}
+}
